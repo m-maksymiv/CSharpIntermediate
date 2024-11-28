28696001aae05929d63b45dc7e001eda5ae9b7ff
Mykola Maksymiv <kolaymaksimiv@gmail.com>
2024-11-28 22:04:54 +0200

    Fields

---
+++ Classes/Fields.cs
@@ -0,0 +1,49 @@
+﻿namespace Classes;
+
+public static class Fields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public readonly List<Order> Orders = new List<Order>();
+
+        public Customer(int id)
+        {
+            Id = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id)
+        {
+            Name = name;
+        }
+
+        public void Promote()
+        {
+            //Orders = new List<Order>();
+            //...
+        }
+    }
+
+    public class Order
+    {
+
+    }
+
+    public static void FieldsProgram()
+    {
+        Console.WriteLine("Start -> Fields");
+
+        var customer = new Customer(1);
+
+        customer.Orders.Add(new Order());
+        customer.Orders.Add(new Order());
+
+        customer.Promote();
+
+        Console.WriteLine(customer.Orders.Count);
+
+        Console.WriteLine("Finish -> Fields");
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -7,6 +7,7 @@
             IntroductionToClasses.IntroductionToClassesProgram();
             Constructors.ConstructorsProgram();
             Methods.MethodsProgram();
+            Fields.FieldsProgram();
         }
     }
 }
